bebd199f993eb97c11ea3489e33b921eb86cc940
Pragasam P <olim@Pragasams-MacBook-Pro.local>
2023-01-09 21:48:27 -0500

    initial checkit

---
+++ SBcallAPI/demo/src/main/java/SBcallAPI/Controller/Controller.java
@@ -0,0 +1,16 @@
+package SBcallAPI.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/test")
+    public String simpleGET()
+    {
+        return "I am simple o ooooo hooooo GET RESPONSE";
+    }
+
+}
+
