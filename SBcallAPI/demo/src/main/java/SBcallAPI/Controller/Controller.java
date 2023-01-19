84f794e8af84a833dbf0cdaf41d414d9803b087f
Pragasam P <olim@Pragasams-MacBook-Pro.local>
2023-01-19 07:30:09 -0500

    changes

--- SBcallAPI/demo/src/main/java/SBcallAPI/Controller/Controller.java
+++ SBcallAPI/demo/src/main/java/SBcallAPI/Controller/Controller.java
@@ -1,15 +1,25 @@
 package SBcallAPI.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller
 {
+    @ResponseBody
     @GetMapping("/test")
-    public String simpleGET()
+    public String simpleGET(@RequestParam String name)
     {
-        return "I am simple o ooooo hooooo GET RESPONSE";
+        String uri = "https://api.nationalize.io/?name=" + name;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        System.out.println(result);
+
+        return name + "     " + result;
     }
 
 }
